c5ca3705f1b73088951490a82234a82acd05ffc1
Tomiwa <ajaomoshood@gmail.com>
2025-01-23 12:07:54 +0100

    mongodb added

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+//middleware//
 require ('dotenv').config();
+mongoose.connect(process.env.DB_URL).then((result) => {
+    console.log("DB isconnected");
+}).catch(err => {
+    console.log(err);
+});
 
 const PORT = process.env.PORT || 5000;
 
